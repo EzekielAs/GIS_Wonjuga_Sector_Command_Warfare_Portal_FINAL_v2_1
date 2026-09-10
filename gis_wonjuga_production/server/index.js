16eaf6707995f7651d146f62c474bcf71ffd3799
EzekielAs <asomaniezekiel8@gmail.com>
2026-09-10 18:35:04 +0000

    keep alive fix

--- gis_wonjuga_production/server/index.js
+++ gis_wonjuga_production/server/index.js
@@ -147,4 +147,6 @@ if (fs.existsSync(publicDir)) {
 }
 
 const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => console.log(`GIS Wonjuga Portal running on ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`GIS Wonjuga Portal running on ${PORT}`);
+});
